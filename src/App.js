1b19b5b8c014ea810af412cbf2662584ff961254
Arvind Purohit <purarvin@amazon.com>
2021-11-14 19:47:34 -0800

    added auth in backend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Arvind Purohit!!</h1>
+        <h1>Wow, Arvind, we now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
